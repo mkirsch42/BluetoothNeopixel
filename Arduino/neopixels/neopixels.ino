1c93e1c1481fad5983767e2d0804601581ae99b1
Mathew Kirschbaum <mkirsch42@gmail.com>
2017-05-19 20:02:30 -0400

    Start working on microphone mode

--- Arduino/neopixels/neopixels.ino
+++ Arduino/neopixels/neopixels.ino
@@ -1,7 +1,7 @@
 #include <Adafruit_NeoPixel.h>
 
 #define NEOPIXEL_PIN 2
-#define NEOPIXEL_COUNT 40
+#define NEOPIXEL_COUNT 24
 
 #define START_CMD 42
 
@@ -11,11 +11,15 @@
 #define OP_ALL   0b00000011
 #define OP_APPLY 0b10000000
 
+#define MODE_BT 0
+#define MODE_LISTEN 1
+
+byte currentMode = MODE_LISTEN;
 Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NEOPIXEL_COUNT, NEOPIXEL_PIN);
 
 void setup() {
   Serial.begin(115200);
-  Serial.setTimeout(60000);
+  Serial.setTimeout(100);
   pixels.begin();
   pixels.show();
 }
@@ -27,24 +31,37 @@ void loop() {
     byte op = in[0] & OP_MASK;
     switch(op) {
       case OP_COLOR:
-        pixels.setPixelColor(in[1], in[2], in[3], in[4]);
+        if(currentMode == MODE_BT) pixels.setPixelColor(in[1], in[2], in[3], in[4]);
         break;
       case OP_RANGE:
-        range(in[1], in[5], in[2], in[3], in[4]);
+        if(currentMode == MODE_BT) range(in[1], in[5], in[2], in[3], in[4]);
         break;
       case OP_ALL:
-        range(0, NEOPIXEL_COUNT - 1, in[2], in[3], in[4]);
+        if(currentMode == MODE_BT) range(0, NEOPIXEL_COUNT - 1, in[2], in[3], in[4]);
         break;
     }
     if(in[0] & OP_APPLY) {
       pixels.show();
     }
   }
+  if(currentMode == MODE_LISTEN) listenMode();
 }
 
 void range(byte s, byte e, byte r, byte g, byte b) {
   for(byte n = s; n <= e; n++) {
     pixels.setPixelColor(n, r, g, b);
   }
+}
+
+
+int peak = 0;
+long peakTime = 0;
+byte[] listenColor = {255, 255, 255};
+#define LISTEN_PEAK_THRESHOLD 170
+void listenMode() {
+  int in = analogRead(0);
+  int pixels = round((NEOPIXEL_COUNT/(float)1024) * in);
+  range(0, pixels, listenColor[0], listenColor[1], listenColor[2]);
+  pixels.show();
 }
 
